b840d10a89d97b30b764cea25a963a323d1f5b8d
Yan Pujante <yan@pongasoft.com>
2018-08-22 13:09:13 -0700

    fixed linkage error on Windows

--- jamba.cmake
+++ jamba.cmake
@@ -258,7 +258,11 @@ include(GoogleTest)
 function(jamba_add_test PROJECT_TEST_NAME TEST_CASES_FILES TEST_SOURCES TEST_LIBS)
   message(STATUS "Adding target ${PROJECT_TEST_NAME} for test cases: ${TEST_CASES_FILES}" )
 
-  add_executable(${PROJECT_TEST_NAME} ${TEST_CASES_FILES} ${TEST_SOURCES})
+  if(WIN)
+    set(WIN_SOURCES "${JAMBA_ROOT}/windows/testmain.cpp")
+  endif()
+
+  add_executable(${PROJECT_TEST_NAME} ${TEST_CASES_FILES} ${TEST_SOURCES} ${WIN_SOURCES})
   target_link_libraries(${PROJECT_TEST_NAME} gtest_main ${TEST_LIBS})
   target_include_directories(${PROJECT_TEST_NAME} PUBLIC ${PROJECT_SOURCE_DIR})
   target_include_directories(${PROJECT_TEST_NAME} PUBLIC ${GTEST_INCLUDE_DIRS})
---
+++ windows/testmain.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 pongasoft
+ *
+ * Licensed under the General Public License (GPL) Version 3; you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author Yan Pujante
+ */
+
+#include <public.sdk/source/main/dllmain.cpp>
+
+/**
+ * The purpose of this file is to satisfies linkage error when compiling tests
+ */
+
+bool InitModule () { return true; }
+bool DeinitModule () { return true; }
