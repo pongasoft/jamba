3805a1d0386e0fd729d61d5f167d11c2caac6b80
Yan Pujante <yan@pongasoft.com>
2020-08-13 16:21:00 -0700

    Fixed vst3_resources.rc file generation

--- cmake/JambaAddVST3Plugin.cmake
+++ cmake/JambaAddVST3Plugin.cmake
@@ -103,7 +103,8 @@ endfunction()
 function(internal_jamba_add_all_resources)
   set(JAMBA_VST3_RESOURCES_RC "")
 
-  get_filename_component(UIDESC_FILENAME "${ARG_UIDESC}" NAME_WLE)
+  get_filename_component(UIDESC_FILENAME ${ARG_UIDESC} NAME_WLE)
+  get_filename_component(UIDESC_DIR ${ARG_UIDESC} DIRECTORY)
 
   internal_jamba_add_resource("${ARG_UIDESC}" "DATA" "" JAMBA_VST3_RESOURCES_RC)
   internal_jamba_add_resources("${JAMBA_VST3_RESOURCES_RC}" JAMBA_VST3_RESOURCES_RC)
@@ -116,9 +117,8 @@ function(internal_jamba_add_all_resources)
   elseif (WIN)
     list(JOIN JAMBA_VST3_RESOURCES_RC "\n" JAMBA_VST3_RESOURCES_RC)
     file(WRITE "${CMAKE_BINARY_DIR}/generated/vst3_resources.rc" ${JAMBA_VST3_RESOURCES_RC})
-    target_sources(${ARG_TARGET} PRIVATE resource/${UIDESC_FILENAME}.rc)
+    target_sources(${ARG_TARGET} PRIVATE "${UIDESC_DIR}/${UIDESC_FILENAME}.rc")
   endif ()
-
 endfunction()
 
 #------------------------------------------------------------------------
@@ -138,7 +138,7 @@ endfunction()
 # internal_jamba_add_resources
 #------------------------------------------------------------------------
 function(internal_jamba_add_resource resource type inList outList)
-  get_filename_component(filename "${resource}" NAME_WLE)
+  get_filename_component(filename "${resource}" NAME)
   smtg_add_vst3_resource(${ARG_TARGET} "${resource}" "")
   file(TO_NATIVE_PATH "${resource}" JAMBA_VST3_RESOURCE_PATH)
   string(REPLACE "\\" "\\\\" JAMBA_VST3_RESOURCE_PATH "${JAMBA_VST3_RESOURCE_PATH}")
