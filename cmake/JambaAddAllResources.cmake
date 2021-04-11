8f9041197b781595bacd60a6f303472ef43a80e5
Yan Pujante <yan@pongasoft.com>
2021-04-11 10:30:49 -0700

    properly account for changes in resource files

--- cmake/JambaAddAllResources.cmake
+++ cmake/JambaAddAllResources.cmake
@@ -20,12 +20,13 @@
 #------------------------------------------------------------------------
 function(jamba_add_all_resources)
   set(JAMBA_VST3_RESOURCES_RC "")
+  set(JAMBA_VST3_RESOURCES_FILE_DEPENDENCY "")
 
   get_filename_component(UIDESC_FILENAME ${ARG_UIDESC} NAME_WLE)
   get_filename_component(UIDESC_DIR ${ARG_UIDESC} DIRECTORY)
 
-  internal_jamba_add_resource("${ARG_UIDESC}" "DATA" "" JAMBA_VST3_RESOURCES_RC)
-  internal_jamba_add_resources("${JAMBA_VST3_RESOURCES_RC}" JAMBA_VST3_RESOURCES_RC)
+  internal_jamba_add_resource("${ARG_UIDESC}" "DATA" "" JAMBA_VST3_RESOURCES_RC "" JAMBA_VST3_RESOURCES_FILE_DEPENDENCY)
+  internal_jamba_add_resources("${JAMBA_VST3_RESOURCES_RC}" JAMBA_VST3_RESOURCES_RC "${JAMBA_VST3_RESOURCES_FILE_DEPENDENCY}" JAMBA_VST3_RESOURCES_FILE_DEPENDENCY)
 
   if (APPLE)
     # processes replacement properties in ARG_MAC_INFO_PLIST_FILE
@@ -42,8 +43,14 @@ function(jamba_add_all_resources)
     endif()
   elseif (WIN32)
     list(JOIN JAMBA_VST3_RESOURCES_RC "\n" JAMBA_VST3_RESOURCES_RC)
-    file(WRITE "${CMAKE_BINARY_DIR}/generated/vst3_resources.rc" ${JAMBA_VST3_RESOURCES_RC})
-    target_sources(${ARG_TARGET} PRIVATE "${UIDESC_DIR}/${UIDESC_FILENAME}.rc")
+    file(WRITE "${CMAKE_BINARY_DIR}/generated/vst3_resources.rc" "${JAMBA_VST3_RESOURCES_RC}\n")
+    add_custom_command(OUTPUT "${CMAKE_BINARY_DIR}/generated/plugin_resources.rc"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${UIDESC_DIR}/${UIDESC_FILENAME}.rc" "${CMAKE_BINARY_DIR}/generated/plugin_resources.rc"
+        COMMAND ${CMAKE_COMMAND} -E touch "${CMAKE_BINARY_DIR}/generated/plugin_resources.rc"
+        DEPENDS "${JAMBA_VST3_RESOURCES_FILE_DEPENDENCY}"
+        VERBATIM
+        )
+    target_sources(${ARG_TARGET} PRIVATE "${CMAKE_BINARY_DIR}/generated/plugin_resources.rc")
   endif ()
 
   if(ARG_RELEASE_SNAPSHOTS)
@@ -56,24 +63,27 @@ endfunction()
 #------------------------------------------------------------------------
 # internal_jamba_add_resources
 #------------------------------------------------------------------------
-function(internal_jamba_add_resources inList outList)
+function(internal_jamba_add_resources inRCContentList outRCContentList inFileDependencyList outFileDependencyList)
   foreach(resource IN LISTS ARG_RESOURCES)
     get_filename_component(type "${resource}" LAST_EXT)
     string(SUBSTRING "${type}" 1 -1 type)
     string(TOUPPER "${type}" type)
-    internal_jamba_add_resource("${resource}" "${type}" "${inList}" inList)
+    internal_jamba_add_resource("${resource}" "${type}" "${inRCContentList}" inRCContentList "${inFileDependencyList}" inFileDependencyList)
   endforeach()
-  set(${outList} "${inList}" PARENT_SCOPE)
+  set("${outRCContentList}" "${inRCContentList}" PARENT_SCOPE)
+  set("${outFileDependencyList}" "${inFileDependencyList}" PARENT_SCOPE)
 endfunction()
 
 #------------------------------------------------------------------------
 # internal_jamba_add_resources
 #------------------------------------------------------------------------
-function(internal_jamba_add_resource resource type inList outList)
+function(internal_jamba_add_resource resource type inRCContentList outRCContentList inFileDependencyList outFileDependencyList)
   get_filename_component(filename "${resource}" NAME)
-  smtg_add_vst3_resource(${ARG_TARGET} "${resource}" "")
+  smtg_add_vst3_resource(${ARG_TARGET} "${resource}")
   file(TO_NATIVE_PATH "${resource}" JAMBA_VST3_RESOURCE_PATH)
+  list(APPEND inFileDependencyList "${JAMBA_VST3_RESOURCE_PATH}")
   string(REPLACE "\\" "\\\\" JAMBA_VST3_RESOURCE_PATH "${JAMBA_VST3_RESOURCE_PATH}")
-  list(APPEND inList "${filename}\t${type}\t\"${JAMBA_VST3_RESOURCE_PATH}\"")
-  set(${outList} ${inList} PARENT_SCOPE)
+  list(APPEND inRCContentList "${filename}\t${type}\t\"${JAMBA_VST3_RESOURCE_PATH}\"")
+  set("${outRCContentList}" "${inRCContentList}" PARENT_SCOPE)
+  set("${outFileDependencyList}" "${inFileDependencyList}" PARENT_SCOPE)
 endfunction()
